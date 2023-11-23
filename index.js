e9dcf78247008f826d5843a54accf6d6ec185276
mew <68041302+iuml@users.noreply.github.com>
2023-11-22 19:48:54 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { Client } = require('discord.js-selfbot-v13');
-const { token , channelid, reply, initialmsg } = require("./config.json")
+const { token , channelid, reply, waittime,  initialmsg } = require("./config.json")
 const client = new Client({
 	checkUpdate: false,
 });
@@ -10,7 +10,7 @@ client.on('ready', async () => {
 
   setInterval(() => {
     chnl.send(initialmsg).then(console.log('Sent.')).catch(err => (console.error(err)))
-  }, 300000);
+  }, waittime);
 })
 client.on('messageCreate', async message => {
 	if(message.guild==null &&message.author.id!==client.user.id){
